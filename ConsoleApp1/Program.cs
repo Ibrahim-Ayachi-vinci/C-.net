584e7aefa6a721fa09c9312f8edad9bae231169a
Ibrahim Ayachi <ibrahim.ayachi@student.vinci.be>
2024-09-28 23:44:28 +0200

    fin jusqu'à 5.3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
 using LINQDataContext;
 
 class Program
@@ -108,5 +109,78 @@ class Program
         {
             Console.WriteLine(result.Request);
         }
+
+
+
+        //////////////////////////////////////////////
+        /// Exerice 4.1
+        /// 
+
+
+
+
+
+        Console.WriteLine("--------------------------------------- Exercice 4.1 ---------------------------------------");
+
+        Console.WriteLine("Resultat annuel moyen : " + dc.Students.Average(st => st.Year_Result));
+
+
+        //////////////////////////////////////////////
+        /// Exerice 4.5
+        /// 
+
+
+
+
+
+        Console.WriteLine("--------------------------------------- Exercice 4.5 ---------------------------------------");
+
+        Console.WriteLine("Nombre de ligns qui composent la table students " + dc.Students.Count());
+
+
+
+
+
+
+
+        //////////////////////////////////////////////
+        /// Exerice 5.1
+        /// 
+
+
+
+        IEnumerable<IGrouping<int, Student>> QueryResult5_1 =
+            from student in dc.Students
+            group student by student.Section_ID;
+
+
+
+        Console.WriteLine("--------------------------------------- Exercice 5.1 ---------------------------------------");
+
+        foreach(IGrouping<int, Student> g in QueryResult5_1)
+        {
+            Console.WriteLine("Resultat moyen pour la section " + g.Key + " est de " + g.Average(student => student.Year_Result));
+        }
+
+
+        //////////////////////////////////////////////
+        /// Exerice 5.1
+        /// 
+
+
+        IEnumerable<IGrouping<int, Student>> QueryResult5_3 =
+            from student in dc.Students
+            where student.BirthDate.Year >=1970 && student.BirthDate.Year <= 1985
+            group student by student.BirthDate.Month;
+
+
+
+        Console.WriteLine("--------------------------------------- Exercice 5.3 ---------------------------------------");
+
+        foreach(IGrouping<int, Student> g in QueryResult5_3)
+        {
+            Console.WriteLine("Résultat moyen pour les étudiant : " + g.Average(student => student.Year_Result) + " née le mois de : " + g.First().BirthDate.Month);
+        }
+
     }
 }
--- ConsoleApp1/obj/Debug/net6.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net6.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f6056be5bcbb1b718917881195927bceda7b8de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d2ea154bb545a69419f54515c27299e8c1b56bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
