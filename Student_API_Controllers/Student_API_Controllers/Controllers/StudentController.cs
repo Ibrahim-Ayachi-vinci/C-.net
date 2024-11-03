a1ed8902f96b6af6ef8b7f1461fc34d7a1085c1d
Ibrahim <ibrahim.ayachi@student.vinci.be>
2024-11-03 15:14:05 +0100

    fin fiche 6

---
+++ Student_API_Controllers/Student_API_Controllers/Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Student_API_Controllers.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private static List<Student> _students = new List<Student>()
+        {
+            new Student { Id = 1, FirstName = "Paul", LastName = "Ochon", Birthdate = new DateTime(1950, 12, 1) },
+            new Student { Id = 2, FirstName = "Daisy", LastName = "Drathey", Birthdate = new DateTime(1970, 12, 1) },
+            new Student { Id = 3, FirstName = "Elie", LastName = "Coptaire", Birthdate = new DateTime(1980, 12, 1) }
+        };
+
+        [HttpGet]
+        public ActionResult<List<Student>> GetAllStudent()
+        {
+            return Ok(_students);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Student> GetStudent(int id) { 
+            
+            var student = _students.FirstOrDefault(x => x.Id == id);
+
+            if (student == null) { 
+                return NotFound();
+            }
+            return Ok(student);
+
+        }
+
+        [HttpPost]
+        public ActionResult PostStudent(Student newStudent) {
+            newStudent.Id = _students.Count() > 0 ? _students.Max(x => x.Id) + 1 : 1;
+
+            _students.Add(newStudent);
+            return Ok();
+        }
+
+
+
+    }
+}
